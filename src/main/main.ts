dc9780205196d5ee90bd5bb858456ffa044e8a0d
jianwei <1807176900@qq.com>
2023-05-22 22:43:54 +0800

    fix: 托盘菜单

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Tray, Menu } = require('electron')
+const path = require('path')
 
 let winURL = process.env.NODE_ENV === 'development'
 ? `http://localhost:9080`
@@ -18,5 +19,25 @@ const createWindow = () => {
 }
 
 app.whenReady().then(() => {
+  // 设置托盘
+  // let iconPath
+  // if (process.env.WEBPACK_DEV_SERVER_URL) {
+  //   // 测试环境
+  //     iconPath = path.join(app.getAppPath(), 'favicon.ico');
+  // }else {
+  //     //正式环境
+  //     iconPath = path.join(path.dirname(app.getPath('exe')), 'favicon.ico');
+  //   }
+  //   console.log('iconPath', iconPath)
+  const iconSrc = path.join(__dirname, '../../public/favicon.ico');
+  const tray = new Tray(iconSrc)
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio' },
+    { label: 'Item2', type: 'radio' },
+    { label: 'Item3', type: 'radio', checked: true },
+    { label: 'Item4', type: 'radio' }
+  ])
+  tray.setToolTip('This is my application.')
+  tray.setContextMenu(contextMenu)
   createWindow()
 })
